fda97427050aa1004f69695303a500bcba9a0d45
Amanda <amanwong@bestbuycanada.ca>
2019-04-21 17:20:27 -0700

    Remove unused attribute

--- src/components/Icons/components/location.jsx
+++ src/components/Icons/components/location.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Location = (props) => {
     return (
-        <svg className="locationIcon" xmlns="http://www.w3.org/2000/svg" width={props.size} height={props.size} viewBox="0 0 24 24" fill={props.colour}>
+        <svg className="locationIcon" width={props.size} height={props.size} fill={props.colour} viewBox="0 0 24 24">
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
             <path d="M0 0h24v24H0z" fill="none" />
         </svg>
